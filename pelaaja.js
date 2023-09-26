b360c4eb6a583c49ee99125024fe0ef9f2244085
mikosavolainen <miko.markus2112@gmail.com>
2023-09-26 21:52:36 +0300

    firebase

--- pelaaja.js
+++ pelaaja.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDt_pFn-B_7boglW4ThHNaFF54_pLkjeNU",
+  authDomain: "farmsim-50260.firebaseapp.com",
+  databaseURL: "https://farmsim-50260-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "farmsim-50260",
+  storageBucket: "farmsim-50260.appspot.com",
+  messagingSenderId: "407008051577",
+  appId: "1:407008051577:web:d15731390ee2f306fb6fa8",
+  measurementId: "G-ZKFZCY3V3E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Listen to the form submit event
 $('#pelaajakirjautuminen').submit(function(evt) {
 
